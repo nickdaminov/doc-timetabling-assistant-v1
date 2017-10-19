62530899c48e06bd7e16ac67d3e5e6605f767e6e
martinxu <myx14@ic.ac.uk>
2017-10-19 13:54:29 +0100

    removed awful code I wrote while half-asleep in index.jsx

--- Frontend/src/client/app/index.jsx
+++ Frontend/src/client/app/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Table from 'react-legit-table'
 import Timetable from './Timetable.jsx';
 
 
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {timetable: [{time: 11, day: "Monday", room: "311", name: "Advanced Databases", age: 2, size: 23}, {time: 12, day: "Monday", room: "311", name: "Advanced Databases", people: 34}]};
+  }
+
   render () {
-    var data = [{time: 11, day: "Monday", room: "311", name: "Advanced Databases", age: 2, size: 23}, {time: 12, day: "Monday", room: "311", name: "Advanced Databases", people: 34}]
-    var monday = {day: "Monday", 9: "", 10: "", 11: "", 12: "", 13: "", 14:"", 15: "", 16: "", 17: ""}
-    var tuesday = {day: "Tuesday", 9:"", 10:"", 11: "", 12: "", 13: "", 14: "", 15: "", 16: "", 17: ""}
-    var rows = [monday]
-    data.forEach(d => {if(d.day = "Monday"){ monday[d.time] = d.name}});
+    var data = this.state.timetable
+    var monday = {day: "Monday"}
+    var tuesday = {day: "Tuesday"}
+    var wednesday = {day: "Wednesday"}
+    var thursday = {day: "Thursday"}
+    var friday = {day: "Friday"}
+    var rows = [monday, tuesday, wednesday, thursday, friday]
+    data.forEach(d => {if(d.day === "Monday"){ monday[d.time] = d.name}
+                       if(d.day === "Tuesday"){tuesday[d.time] = d.name}
+                       if(d.day === "Wednesday"){wednesday[d.time] = d.name}
+                       if(d.day === "Thursday"){thursday[d.time] = d.name}
+                       if(d.day === "Friday"){friday[d.time] = d.name}});
     console.log(monday);
     return <Timetable rows={rows}/>
   }
