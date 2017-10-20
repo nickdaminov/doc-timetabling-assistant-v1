b8ca21e21d91b45886770a534242a5998ef8261b
martinxu <myx14@ic.ac.uk>
2017-10-20 12:16:09 +0100

    added more dummy data to initial state of frontend

--- Frontend/src/client/app/index.jsx
+++ Frontend/src/client/app/index.jsx
@@ -8,7 +8,20 @@ import Timetable from './Timetable.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {timetable: [{time: 11, day: "Monday", room: "311", name: "Advanced Databases", age: 2, size: 23}, {time: 12, day: "Monday", room: "311", name: "Advanced Databases", people: 34}]};
+    this.state = {timetable: [{time: 11, day: "Monday", room: "311", name: "Advanced Databases"}, {time: 12, day: "Monday", room: "311", name: "Advanced Databases"},
+                              {time: 9, day: "Monday", room: "311", name: "Operations Research"}, {time: 10, day: "Monday", room: "311", name: "Operations Research"},
+                              {time: 16, day: "Monday", room: "311", name: "Information and Coding"},{time: 17, day: "Monday", room: "311", name: "Information and Coding"},
+                              {time: 9, day: "Tuesday", room: "311", name: "Robotics"},{time: 10, day: "Tuesday", room: "311", name: "Robotics"},
+                              {time: 11, day: "Tuesday", room: "311", name: "Simulation and Modelling"}, {time: 12, day: "Tuesday", room: "311", name: "Simulation and Modelling"},
+                              {time: 14, day: "Tuesday", room: "311", name: "Type Systems"}, {time: 15, day: "Tuesday", room: "311", name: "Type Systems"},
+                              {time: 16, day: "Tuesday", room: "311", name: "Computer Vision"}, {time: 17, day: "Tuesday", room: "311", name: "Computer Vision"},
+                              {time: 9, day: "Thursday", room: "311", name: "Information and Coding"}, {time: 10, day: "Thursday", room: "311", name: "Information and Coding"},
+                              {time: 11, day: "Thursday", room: "311", name: "Robotics"}, {time: 12, day: "Thursday", room: "311", name: "Robotics"},
+                              {time: 14, day: "Thursday", room: "311", name: "Operations Research"}, {time: 15, day: "Thursday", room: "311", name: "Operations Research"},
+                              {time: 9, day: "Friday", room: "311", name: "Computer Vision"}, {time: 10, day: "Friday", room: "311", name: "Computer Vison"},
+                              {time: 11, day: "Friday", room: "311", name: "Simulation and Modelling"}, {time: 12, day: "Friday", room: "311", name: "Simulation and Modelling"},
+                              {time: 14, day: "Friday", room: "311", name: "Type Systems"}, {time: 15, day: "Friday", room: "311", name: "Type Systems"},
+                              {time: 16, day: "Friday", room: "311", name: "Advanced Databases"}, , {time: 17, day: "Friday", room: "311", name: "Advanced Databases"}]};
   }
 
   render () {
